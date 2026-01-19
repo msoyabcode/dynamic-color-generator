8bf3279fda09ad229c5fcef1f8a88f78cd64d2b2
msoyabcode <mohdsoyab0785@gmail.com>
2026-01-19 21:30:55 +0530

    Added color history feature with duplicate prevention

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
 
 const [color, setColor] = useState("#FFFFFF")
 const [copy, setCopy] = useState("")
+const [history, sethistory] = useState([])
 
 const colorChange = () =>{
   const hexColrs = "123456789ABCDEF"
@@ -12,8 +13,19 @@ const colorChange = () =>{
     const randomColrs = Math.floor(Math.random()*16)
     newColors+=hexColrs[randomColrs]
   }
+  if(newColors===color){
+    return 
+  } 
   setColor(newColors)
+
+  sethistory(prev=>{
+    if(prev.includes(newColors)){
+      return prev
+    }
+   return [...prev, newColors ]})
+
 }
+
 
 const copyColor = async () =>{
   try{
@@ -29,11 +41,19 @@ const copyColor = async () =>{
 
   return (
     <div>
-      <div className='h-screen flex flex-col justify-center items-center transition-color duration-500 ' style={{backgroundColor: color}}>
+      <div className='h-screen flex flex-col justify-center items-center transition-colors duration-500 ' style={{backgroundColor: color}}>
         <h1 className='text-2xl mb-6 font-bold transition duration-300 '>{color}</h1>
         <div className='mb-4 flex flex-col items-center'>
            <button className='text-lg font-medium bg-white py-2 px-4 rounded-2xl shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer' onClick={()=>copyColor()}>Copy Color Code</button>
            {copy&& <span className='mt-2 text-white font-semibold'>{copy}</span>}
+          </div>
+          <div className='flex gap-2 mt-6 mb-5 flex-wrap p-11 '>
+         { history.map((historyColor, index) => (
+          <div className='w-8 h-8 border-2' key={index}
+          style={{backgroundColor: historyColor}}
+          onClick={()=> setColor(historyColor)}>
+          </div>
+          ))}
           </div>
         <button className='text-3xl font-medium bg-white py-2 px-2 rounded-2xl shadow-2xl transition-shadow duration-300 cursor-pointer' onClick={()=>colorChange()}>Click me to change color </button>
       </div>
